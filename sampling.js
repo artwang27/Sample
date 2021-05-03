014d670f320d00c5d3cc9389353136e1fadd0009
artwang27 <71919659+artwang27@users.noreply.github.com>
2021-05-03 23:07:34 +0800

    0503

--- sampling.js
+++ sampling.js
@@ -2,84 +2,120 @@
 */
 
 
-
-class Font{
-   static Width=5;
-   static Height=7;
-   static TrueColor;
+class Font {
+   static Width = 5;
+   static Height = 7;
+   //static TrueColor;
    //static FalseColor;
-
-   constructor(width, height){
-      this.width=width;
-      this.height=height;
-      TrueColor = color("Black");
-      FalseColor = color("White");
-
+   static setupTrueColor(c) {
+      Font.TrueColor = c.levels;  //把 color 轉換成 rgba 陣列
    }
-   
+
+   static setupFalseColor(c) {
+      Font.FalseColor = c.levels;  //把 color 轉換成 rgba 陣列
+   }
+
+
 }
 
-function setTrueColor(c){
-   Font.TrueColor=c;
-}
+class Letter {
+   constructor() {
+      this.bits = [];
+   }
 
-/*
-function setFalseColor(c) {
-   Font.FalseColor = c;
-}
-*/
+   //------------------------------------------------------------------
+   //從 bitmap 的某一區間，萃取字體，構成 colors 陣列
+   asColors(startX, startY, dx, dy) {
+      let colors = this.getLetter(startX, startY, dx, dy, null);
+      //console.log(colors);
+      return colors;
+   }
 
 
-//從 bitmap 的某一區間，萃取字體，構成 bool 陣列
-function getLetter(startX, startY, dx, dy, colorAs=null) {
-   let bits=[];   //array of bool
-   let y=startY;
-   for(let j=0; j<Font.Height; j++, y+=dy){
-      let x=startX;
+   //------------------------------------------------------------------
+   //從 bitmap 的某一區間，萃取字體，構成 bool 陣列，並存入 this.bits 方便未來的處理
+   //compareFunction：可在 Letter 類別之外另行撰寫，並以函式指標參數傳入
+   //也可以省略此參數，以預設的 compareFontTrueColor 做顏色的比對
+   asBools(startX, startY, dx, dy, compareFunction=this.compareFontTrueColor) {
+      this.bits = this.getLetter(startX, startY, dx, dy, compareFunction);
+      return this.bits; //array of bool
+   }
 
-      for(let i=0; i<Font.Width; i++, x+=dx){
+   //判斷顏色若是與 TrueColor 相符，則傳回 true
+   compareFontTrueColor(c) {
+      //return colorEqual(c, Font.TrueColor);
+      return ! colorEqual(c, Font.FalseColor);
+   }
+   //------------------------------------------------------------------
 
-         let result = extract(x, y, dx, dy, colorAs);
-         bits.push(result);
-
+   //把 bool 陣列轉換為字串陣列
+   //當 true 時轉換為 trueChar，
+   //當 false 時轉換為 falseChar
+   toStrings(trueChar, falseChar){
+      let strAry=[];
+      if(this.bits.length===0){
+         return "null";
       }
+
+      let idx=0;
+      for (let j = 0; j < Font.Height; j++) {
+         let str="";
+         for (let i = 0; i < Font.Width; i++) {
+            str += this.bits[idx++] ? trueChar : falseChar;
+         }
+         strAry.push(str);
+      }
+
+      return strAry;
    }
 
-   console.log(bits);
-}
+
+   //從 bitmap 的某一區間，萃取字體，構成 bool 陣列
+   getLetter(startX, startY, dx, dy, compareFunction = null) {
+      let bits = [];   //array of bool
+      let y = startY;
+      for (let j = 0; j < Font.Height; j++, y += dy) {
+         let x = startX;
+
+         for (let i = 0; i < Font.Width; i++, x += dx) {
+
+            let result = this.extract(x, y, dx, dy, compareFunction);
+            bits.push(result);
+
+         }
+      }
+
+      //console.log(bits);
+      return bits;
+   }
+
+   //預設：傳回陣列(裡面有四個元素，分別代表 r,g,b,a)
+   extract(x, y, dx, dy, compareFunction) {
+      let c = get(x, y);   //萃取顏色
+      paintBox(x, y, dx, dy, c);
+
+      //若 colorAs 有定義，則繼續執行 colorAs 函式，否則直接傳回陣列(裡面有四個元素，分別代表 r,g,b,a)
+      if (compareFunction) return compareFunction(c);
+      else return c;
+   }
 
 
-//預設：傳回陣列(裡面有四個元素，分別代表 r,g,b,a)
-function extract(x,y,dx,dy,colorAs){
-   let c = get(x, y);   //萃取顏色
 
-   paintBox(x, y, dx, dy,c);
+}//class 
 
-   //若 colorAs 有定義，則繼續執行 colorAs 函式，否則直接傳回陣列(裡面有四個元素，分別代表 r,g,b,a)
-   if(colorAs) return colorAs(c);
-   else        return c;
-}
-
-
-//判斷顏色若是與 TrueColor 相符，則傳回 true
-function colorAsBool(_color){
-   //const TrueColor=color("Black");
-   const TrueColor = color("White").levels;
-
-   return colorEqual( _color, TrueColor);
-}
 
 //在取出的範圍，塗上色塊
-function paintBox(x,y,dx,dy,c) {
+function paintBox(x, y, dx, dy, c) {
+   let r=0.8;
    fill(c);
    noStroke();
-   rect(x, y, dx, dy);
-   //print(x,y);
+   rect(x, y, dx*r, dy*r);
 }
+
 
 
 //比對顏色，使用陣列的元素比對
-function colorEqual(a,b){
-   return a[0]==b[0] && a[1]==b[1] && a[2]==b[2] && a[3]==b[3];
+function colorEqual(a, b) {
+   return a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && a[3] == b[3];
 }
 
--- sketch.js
+++ sketch.js
@@ -1,33 +1,70 @@
 function setup() {
-  // put setup code here
-    createCanvas(1920, 1080);
-    image(img, 0, 0);
+	// put setup code here
+	createCanvas(800, 600);
+	image(img, 0, 0);
 
-    print("---");
-    //setTrueColor("White");
-    Font.TrueColor=color("White").levels;
-    print(Font.TrueColor);
-    print("---");
-    getLetter(0,0,80,80,colorAsBool);
+	test();
+	return;
+	getLetter(0, 0, 80, 80, colorAsBool);
 }
 
-function draw() {
-  // put drawing code here
-    //getLetter(0, 0, 50, 50);
-    return;
 
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
+
+function draw() {
+	// put drawing code here
+	//getLetter(0, 0, 50, 50);
+	return;
+
+	if (mouseIsPressed) {
+		fill(0);
+	} else {
+		fill(255);
+	}
+	ellipse(mouseX, mouseY, 80, 80);
 }
 
 let img;
 function preload() {
-  img = loadImage('assets/tiles.png');
+	img = loadImage('assets/eng02.png');
 }
 
 
+
+function test() {
+	print("---");
+
+	//Font.setupTrueColor("White");
+	//Font.TrueColor=color("White").levels;
+	let c= color(245,218,4);
+	Font.setupTrueColor( c);
+	c=color("Black");
+	Font.setupFalseColor(c);
+
+	print(Font.TrueColor);
+	print("---");
+
+	let L=new Letter();
+	//let colors=L.asColors(2, 2, 54, 54);
+	//print(colors);
+
+	translate(350, 0);
+	L.asBools(23,14,5,5);
+	print(L.bits);
+
+	const space=""
+	let strAry = L.toStrings('O',' ');
+	print(strAry);
+
+	printLetter(strAry);
+
+}
+
+function printLetter(strAry){
+	let s="";
+	for(let i=0; i< strAry.length; i++){
+		s = s+ strAry[i] + "<BR>";
+	}
+
+	document.getElementById("textArea").innerHTML = s;
+}
 
